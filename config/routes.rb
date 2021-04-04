067734e5c82fc5aa0b67b865cd14bbafca9f12bd
Kevin Wang <kevin.wang11@outlook.com>
2021-04-04 00:46:02 -0400

    added template from https://github.com/ucfopen/lti-template-rails

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  root 'application#index'
+
+  # LTI XML CONFIG
+  get :lti_config, controller: :application, as: :lti_config
+
+  # LTI LAUNCH URL (responds to get and post)
+  match 'launch' => 'application#launch', via: [:get, :post], as: :lti_launch
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  post 'grade/:score', to: 'application#grade', as: :lti_grade
+end
