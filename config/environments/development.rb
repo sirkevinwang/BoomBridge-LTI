af1fc265dcdbca4fcb158143eaa918a621af1061
caitlinhuang <caitlinhuang8@gmail.com>
2021-04-10 11:48:55 -0700

    added views

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
-  config.hosts << "https://stale-otter-20.loca.lt"
+  config.hosts << "loud-treefrog-99.loca.lt"
   # Do not eager load code on boot.
   config.eager_load = false
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-  config.hosts << "https://stale-otter-20.loca.lt"
+  config.hosts << "loud-treefrog-99.loca.lt"
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
