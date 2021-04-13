295f96da6bdfb917eb4488dfe8a1ad5c5c1d4527
Kevin Wang <kevin.wang11@outlook.com>
2021-04-13 10:32:59 -0400

    Confetti now works

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,7 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
   config.hosts << "boombridge.herokuapp.com"
+  config.hosts << ".loca.lt"
   # Do not eager load code on boot.
   config.eager_load = false
 
